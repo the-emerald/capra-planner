41f7321f83533c148193ca1aabc68e1ea8bc415d
Anson <git@anson-cheung.com>
2020-05-30 05:17:53 +0800

    Add vuex

--- planner/src/main.ts
+++ planner/src/main.ts
@@ -4,6 +4,7 @@ import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import store from './store'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app');
