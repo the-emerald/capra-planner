5db2730b5393d662d3858cb011fe9efa9ad74d96
Anson <git@anson-cheung.com>
2020-06-27 23:30:50 +0800

    Add constraints and more information

--- migrations/2020-06-27-100802_init/up.sql
+++ migrations/2020-06-27-100802_init/up.sql
@@ -1,89 +1,101 @@
 -- Your SQL goes here
 CREATE TABLE Users (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    name text,
-    tissue_state text,
-    current_zhl_setting_id integer,
-    current_vpm_setting_id integer,
-    current_gas_plan_setting_id integer
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    current_tissue INTEGER NOT NULL, -- fk
+    current_zhl_setting_id INTEGER NOT NULL, -- fk
+    current_vpm_setting_id INTEGER NOT NULL, -- fk
+    current_gas_plan_setting_id INTEGER NOT NULL -- fk
 );
 
 CREATE TABLE ZHLSettings (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    gfl integer,
-    gfh integer,
-    ascent_rate integer,
-    descent_rate integer
-);
-
-CREATE TABLE GasPlanSettings (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    sac_bottom integer,
-    sac_deco integer
-);
-
-CREATE TABLE Dives (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    user_id integer,
-    deco_gases text,
-    timestamp datetime,
-    zhl_settings_id integer,
-    vpm_settings_id integer,
-    gas_plan_setting_id integer
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    gfl INTEGER NOT NULL,
+    gfh INTEGER NOT NULL,
+    ascent_rate INTEGER NOT NULL,
+    descent_rate INTEGER NOT NULL
 );
 
 CREATE TABLE VPMSettings (
-    id integer PRIMARY KEY AUTOINCREMENT
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
 );
 
+CREATE TABLE GasPlanSettings (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    sac_bottom INTEGER NOT NULL,
+    sac_deco INTEGER NOT NULL
+);
+
+CREATE TABLE Dives (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,    -- fk
+    deco_gases TEXT NOT NULL,
+    tissue_before_id INTEGER NOT NULL,   -- fk
+    timestamp DATETIME NOT NULL,
+    zhl_settings_id INTEGER NOT NULL,    -- fk
+    vpm_settings_id INTEGER NOT NULL,    -- fk
+    gas_plan_setting_id INTEGER NOT NULL -- fk
+);
+
+
 CREATE TABLE Segments (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    dive_id integer,
-    start_depth integer,
-    end_depth integer,
-    time integer,
-    gas_id integer
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    dive_id INTEGER NOT NULL,   -- fk
+    start_depth INTEGER NOT NULL,
+    end_depth INTEGER NOT NULL,
+    time INTEGER NOT NULL,
+    gas_id INTEGER NOT NULL -- fk
 );
 
 CREATE TABLE Gases (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    o2 integer,
-    he integer,
-    dive_id integer
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    dive_id INTEGER NOT NULL -- fk
+    o2 INTEGER NOT NULL,
+    he INTEGER NOT NULL,
 );
 
 CREATE TABLE Tissues (
-    user_id integer,
-    n2_1 integer,
-    n2_2 integer,
-    n2_3 integer,
-    n2_4 integer,
-    n2_5 integer,
-    n2_6 integer,
-    n2_7 integer,
-    n2_8 integer,
-    n2_9 integer,
-    n2_10 integer,
-    n2_11 integer,
-    n2_12 integer,
-    n2_13 integer,
-    n2_14 integer,
-    n2_15 integer,
-    n2_16 integer,
-    he_1 integer,
-    he_2 integer,
-    he_3 integer,
-    he_4 integer,
-    he_5 integer,
-    he_6 integer,
-    he_7 integer,
-    he_8 integer,
-    he_9 integer,
-    he_10 integer,
-    he_11 integer,
-    he_12 integer,
-    he_13 integer,
-    he_14 integer,
-    he_15 integer,
-    he_16 integer
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    n2_1 REAL NOT NULL,
+    n2_2 REAL NOT NULL,
+    n2_3 REAL NOT NULL,
+    n2_4 REAL NOT NULL,
+    n2_5 REAL NOT NULL,
+    n2_6 REAL NOT NULL,
+    n2_7 REAL NOT NULL,
+    n2_8 REAL NOT NULL,
+    n2_9 REAL NOT NULL,
+    n2_10 REAL NOT NULL,
+    n2_11 REAL NOT NULL,
+    n2_12 REAL NOT NULL,
+    n2_13 REAL NOT NULL,
+    n2_14 REAL NOT NULL,
+    n2_15 REAL NOT NULL,
+    n2_16 REAL NOT NULL,
+    he_1 REAL NOT NULL,
+    he_2 REAL NOT NULL,
+    he_3 REAL NOT NULL,
+    he_4 REAL NOT NULL,
+    he_5 REAL NOT NULL,
+    he_6 REAL NOT NULL,
+    he_7 REAL NOT NULL,
+    he_8 REAL NOT NULL,
+    he_9 REAL NOT NULL,
+    he_10 REAL NOT NULL,
+    he_11 REAL NOT NULL,
+    he_12 REAL NOT NULL,
+    he_13 REAL NOT NULL,
+    he_14 REAL NOT NULL,
+    he_15 REAL NOT NULL,
+    he_16 REAL NOT NULL
 );
+
+
+-- Default values for settings
+-- ZHL-16
+INSERT INTO ZHLSettings VALUES (1, 50, 70, 20, -10)
+
+-- VPM
+
+-- Gas Planning
+INSERT INTO GasPlanSettings VALUES (1, 20, 15)
