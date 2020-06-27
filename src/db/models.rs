7e0e2e1cf4a3ae13db93455b87aca8cac12b0e1f
Anson <git@anson-cheung.com>
2020-06-28 03:31:59 +0800

    Fix naming and add gas plan setting

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,9 +1,11 @@
-use crate::db::schema::{users, zhl_settings, vpm_settings};
+use crate::db::schema::{users, zhl_settings, vpm_settings, gas_plan_settings};
 
 #[derive(Queryable, Identifiable, Associations)]
 #[table_name = "users"]
-#[belongs_to(parent = "ZHLSettings", foreign_key = "current_zhl_setting_id")]
-#[belongs_to(parent = "VPMSettings", foreign_key = "current_vpm_setting_id")]
+#[belongs_to(parent = "ZHLSetting", foreign_key = "current_zhl_setting_id")]
+#[belongs_to(parent = "VPMSetting", foreign_key = "current_vpm_setting_id")]
+#[belongs_to(parent = "GasPlanSetting", foreign_key = "current_gas_plan_setting_id")]
+
 pub struct User {
     pub id: i32,
     pub name: String,
@@ -15,7 +17,7 @@ pub struct User {
 
 #[derive(Queryable, Identifiable)]
 #[table_name = "zhl_settings"]
-pub struct ZHLSettings {
+pub struct ZHLSetting {
     pub id: i32,
     pub gfl: i32,
     pub gfh: i32,
@@ -25,6 +27,14 @@ pub struct ZHLSettings {
 
 #[derive(Queryable, Identifiable)]
 #[table_name = "vpm_settings"]
-pub struct VPMSettings {
+pub struct VPMSetting {
     pub id: i32,
+}
+
+#[derive(Queryable, Identifiable)]
+#[table_name = "gas_plan_settings"]
+pub struct GasPlanSetting {
+    pub id: i32,
+    pub sac_bottom: i32,
+    pub sac_deco: i32,
 }
