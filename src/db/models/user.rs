b9420b6b12c78a020f4c30b2214dcdbd17496eee
Anson <git@anson-cheung.com>
2020-06-28 21:58:19 +0800

    Add new user route

--- src/db/models/user.rs
+++ src/db/models/user.rs
@@ -3,7 +3,7 @@ use crate::db::models::settings::*;
 use crate::db::models::tissue::Tissue;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Identifiable, Associations)]
+#[derive(Queryable, Identifiable, Associations, Serialize, Deserialize)]
 #[table_name = "users"]
 #[belongs_to(parent = "Tissue", foreign_key = "current_tissue_id")]
 #[belongs_to(parent = "ZHLSetting", foreign_key = "current_zhl_settings_id")]
@@ -18,7 +18,8 @@ pub struct User {
     pub current_gas_plan_settings_id: i32
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Insertable)]
+#[table_name = "users"]
 pub struct NewUser {
     pub name: String,
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,10 +1,21 @@
 use crate::{DBPool, db};
 use actix_web::{post, web, HttpResponse};
+use crate::db::models;
 
 #[post("/user/new")]
 pub(crate) async fn add_user(
     pool: web::Data<DBPool>,
-    form: web::Json<db::models::user::NewUser>
+    form: web::Json<models::user::NewUser>
 ) -> actix_web::Result<HttpResponse> {
-    unimplemented!()
+    let conn = pool
+        .get()
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+    let user = web::block(move || db::actions::insert_new_user(&form, &conn))
+        .await
+        .map_err(|e| {
+            eprintln!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })?;
+    Ok(HttpResponse::Ok().json(user))
 }
