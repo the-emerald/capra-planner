4318bf61489f72fa879efb599c0c83f5c9dc95c4
Anson <git@anson-cheung.com>
2020-06-30 20:03:14 +0800

    Improve error messages

--- planner/src/common/axios_error.ts
+++ planner/src/common/axios_error.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
 export function handleAxiosError(error: any): string {
     if (error.response) {
-        return "Server encountered an error."
+        return `Server encountered an error. (${error.response.status})`
     }
     else if (error.request) {
-        return "Could not contact server."
+        return "Could not contact server. Please make sure it is reachable."
     }
     else {
-        return "Please complain to Emerald about it."
+        return "Something went seriously wrong. Please complain to Emerald about this."
     }
 }
