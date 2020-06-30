d1c41ad046f7ecb17d4514d4bca2efdfc98d8950
Anson <git@anson-cheung.com>
2020-06-30 22:54:54 +0800

    Refactor insert new user to use transactions

--- src/db/actions/user.rs
+++ src/db/actions/user.rs
@@ -1,26 +1,22 @@
 use diesel::{SqliteConnection, RunQueryDsl};
 use crate::db::models;
-use crate::db::models::user::NewUser;
+use crate::db::models::user::{NewUser, User};
 use diesel::prelude::*;
 
 pub fn insert_new_user(new_user: &NewUser,
                        conn: &SqliteConnection
 ) -> Result<models::user::User, diesel::result::Error> {
-    // Unfortunately SQLite does not allow for get_result(conn), so we are forced to make a query
-    // to insert the new user and then fetch the row we just created.
     use crate::db::schema::users::dsl::*;
 
-    // Insert
-    diesel::insert_into(users)
-        .values(new_user)
-        .execute(conn)?;
+    conn.transaction::<User, diesel::result::Error, _>(|| {
+        diesel::insert_into(users)
+            .values(new_user)
+            .execute(conn)?;
 
-    // Fetch the new user
-    let user = users
-        .filter(name.eq(&new_user.name))
-        .first::<models::user::User>(conn)?;
-
-    Ok(user)
+        users
+            .filter(name.eq(&new_user.name))
+            .first::<models::user::User>(conn)
+    })
 }
 
 pub fn get_user_by_id(id_: i32,
