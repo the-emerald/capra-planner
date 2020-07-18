bc0315cb98d0ae424a56686d280a62cb7c8c84b8
Anson <git@anson-cheung.com>
2020-07-19 03:20:13 +0800

    Add plan results component

---
+++ planner/src/components/plan/results/Results.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <b-card-group>
+            <b-card no-body>
+                <b-card-header>
+                    <b-container class="p-0">
+                        <b-form-row>
+                            <b-col sm="6" style="margin-top: 0.2rem">
+                                Results
+                            </b-col>
+                            <!--UI buttons-->
+
+                            <!--Reorder-->
+                            <b-col>
+                                <b-button block
+                                          class="float-right" id="rearrange"
+                                          size="sm"
+                                          title="Reorder">
+                                    <b-icon-list-ol></b-icon-list-ol>
+                                </b-button>
+                            </b-col>
+
+                        </b-form-row>
+                    </b-container>
+
+                    <!--Relevant modals-->
+                </b-card-header>
+                <b-container>
+                    <p>Reserved for showing dive results bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg bleg</p>
+                </b-container>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({})
+    export default class Results extends Vue {
+
+    }
+
+
+</script>
+
+<style scoped>
+    .list_group {
+        height: 75vh;
+        overflow-y: auto;
+    }
+</style>
--- planner/src/views/Plan.vue
+++ planner/src/views/Plan.vue
@@ -2,19 +2,19 @@
     <div class="plan">
         <Navbar></Navbar>
         <br>
-        <b-container class="float-left">
+        <b-container fluid="true" class="ml-3 mr-3">
             <b-row>
-                <b-col class="col-4">
+                <b-col cols="3">
                     <!--b-list group for dive segments-->
                     <BottomSegments></BottomSegments>
                 </b-col>
-                <b-col class="col-4">
+                <b-col cols="3">
                     <!--b-list group for deco gases-->
                     <DecoGases></DecoGases>
                 </b-col>
-                <b-col>
+                <b-col cols="6">
                     <!--reserved for showing dive results-->
-                    <p>Reserved for showing dive results</p>
+                    <Results></Results>
                 </b-col>
             </b-row>
         </b-container>
@@ -26,10 +26,12 @@
     import {Component, Vue} from "vue-property-decorator";
     import BottomSegments from "@/components/plan/bottom_segments/BottomSegments.vue";
     import DecoGases from "@/components/plan/deco_gases/DecoGases.vue";
+    import Results from "@/components/plan/results/Results.vue";
 
     @Component({
         name: "Plan",
         components: {
+            Results,
             DecoGases,
             BottomSegments,
             Navbar,
